ea48e7d2ed5978eb3b47ae3d43ac8e57de0b0218
Richard Eric van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2021-06-24 12:25:04 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/cloudBenchmarking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloudBenchmarking.dir/main.cpp.o"
+  "cloudBenchmarking"
+  "cloudBenchmarking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cloudBenchmarking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
